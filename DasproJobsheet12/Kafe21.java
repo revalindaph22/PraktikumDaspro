85ddd8fa1576206e6353c1d4d377cad84b852790
revalinda <revalindaph@gmail.com>
2025-11-29 20:18:12 +0700

    Percobaan 2 Fungsi  1

--- DasproJobsheet12/Kafe21.java
+++ DasproJobsheet12/Kafe21.java
@@ -1,7 +1,15 @@
 package DasproJobsheet12;
 
+import java.net.Socket;
+// deklarasi Fungsi harus sebelum method main
 public class Kafe21 { // mendeklarasi sebuah class bernama Kafe21 (wadah kode)
-    public static void main(String[] args) { // method main, pintu masuk program java
+    public static void Menu(String namaPelanggan, boolean isMember) { // mendefinisikan method bernama Menu dengan parameter bernilai teks (nama) dan true/false
+        System.out.println("Selamat datang, " + namaPelanggan + "!"); // print sapaan pelanggan dengan memasukkan parameter nama pelanggan
+
+        if (isMember) { // chek apakah pelanggan adalah member (isMember = True)
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        } // jika true print diskon, jika false tidak di print (di lewati)
+
         System.out.println("==== MENU RESTO KAFE ===="); // mencetak ke layar
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Capuccino - Rp 20,000");
@@ -11,30 +19,8 @@ public class Kafe21 { // mendeklarasi sebuah class bernama Kafe21 (wadah kode)
         System.out.println("6. Capuccino - Rp 20,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang Anda inginkan.");
+    } // menutup blok menu
+    public static void main(String[] args) { // method main, pintu masuk program java
+        Menu("Andi", true); // memanggil method Menu, dengan menambahkan parameter namaPelanggan dan True untuk isMember
     }
 }
-
-/* PERTANYAAN PERCOBAAN 1
-1. Apakah fungsi tanpa parameter selalu harus bertipe void?
-    Fungsi tanpa parameter tidak harus bertipe void, karena parameter dan return type itu dua hal yang
-    berbeda. Parameter berhubungan dengan "apakah fungsi menerima input", sedangkan Return type berhubungan
-    dengan "apakah fungsi menghasilkan output". Jadi meskipun tidak punya parameter, fungsi tetap bisa
-    mengembalikan nilai, sehingga tidak harus void.
-2. Apakah daftar menu pada program kafe dapat ditampilkan tanpa menggunakan fungsi 
-Menu()? Modifikasi kode program tersebut untuk dapat menampilkan daftar menu 
-tanpa menggunakan fungsi!
-    Bisa, Menu bisa ditampilkan tanpa menggunakan fungsi Menu(), langsung tuliskan System.out.println()
-    di dalam main(). Modification Done.
-3. Jelaskan keuntungan menggunakan fungsi Menu() dibandingkan menulis semua 
-perintah penampilan menu langsung di dalam fungsi main. 
-    Kode lebih rapi, tidak ribet mengulangi baris print jika ingin dipanggil, memudahkan membacanya,
-    memudahkan pengembangan program jika program makin besar, memudahkan pemeliharaan (maintenance) 
-    jika ada menu baru
-4. Uraikan secara singkat alur eksekusi program ketika fungsi Menu() dipanggil dari main 
-(mulai dari program dijalankan sampai daftar menu tampil di layar). 
-    Membuat class bernama Kafe21 sebagai wadah program, lalu mendefinisi sebuah method (kumpulan perintah) bernama Menu,
-    lalu memanggil S.O.P untuk mencetak judul menu, menu ke 1,2,3,4,5,6, garis pembatas, dan instruksi ke user untuk 
-    memilih menu ke layar, } menutup blok method Menu. Selanjutnya menjalankan method main (pintu masuk utama java) maka
-    progam akan mulai berjalan dan setelahnya memanggil method Menu tanpa parameter/objek, } menutup blok method main,
-    } menutup blok class Kafe21, setelah itu program di run dan keluar output an menu kafe.
-*/
