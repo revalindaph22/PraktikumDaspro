278034a3918372a35bc4336a45c5342bb545fa1c
revalinda <revalindaph@gmail.com>
2025-11-30 11:39:54 +0700

    Percobaan 4 Fungsi 1 Final

--- DasproJobsheet12/PengunjungCafe21.java
+++ DasproJobsheet12/PengunjungCafe21.java
@@ -17,12 +17,15 @@ public class PengunjungCafe21 {
 
 /* PERTANYAAN PERCOBAAN 4
 1. Jelaskan mengapa penulisan parameter di praktikum 4 ditulis dengan String...namaPengunjung!
-    Karena String... adalah varags (variabel arguments) yang memungkinkan method menerima jumlah parameter yang tidak terbatas
+    Karena String... adalah varargs (variabel arguments) yang memungkinkan method menerima jumlah parameter yang tidak terbatas
     sehinggan pemanggilan bisa fleksibel atau bahkan tanpa jumlah tetap. String... membuat kode lebih ringkas dan fleksibel.
 2. Modifikasi method daftarPengunjung menggunakan for-each loop. Modification Done.
 3. Bisakah menggunakan dua tipe data varaargs dalam satu fungsi? Jelaskan jawaban 
-Anda berdasarkan aturan varargs di Java, dan berikan contohnya! 
+Anda berdasarkan aturan varargs di Java, dan berikan contohnya!
+    Tidak boleh lebih dari satu varargs (...) dalam satu method, java akan error karena varargs harus menjadi parameter terakhir.
 4. Jelaskan apa yang terjadi jika fungsi daftarPengunjung dipanggil tanpa argumen. 
 Apakah program akan error saat kompilasi, error saat dijalankan, atau tetap berjalan? 
 Jika tetap berjalan, bagaimana output yang dihasilkan? 
+    Program tidak akan error dan akan tetap berjalan, karena varargs memperlakukan input sebagai array kosong ([])
+    jadi hanya akan menampilkan daftar kosong "Daftar Nama Pengunjung: " saja yang ditampilkan.
 */
