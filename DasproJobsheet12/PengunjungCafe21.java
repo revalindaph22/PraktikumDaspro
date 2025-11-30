b4985ce84a74ceb478004d9bfd7dc7415f9a08fd
revalinda <revalindaph@gmail.com>
2025-11-30 09:20:43 +0700

    Percobaan 4 Fungsi 1

---
+++ DasproJobsheet12/PengunjungCafe21.java
@@ -0,0 +1,16 @@
+package DasproJobsheet12;
+
+public class PengunjungCafe21 {
+
+    public static void daftarPengunjung(String...namaPengunjung){
+        System.out.println("Daftar Nama Pengunjung:");
+        for (int i = 0; i < namaPengunjung.length; i++) {
+            System.out.println("- " + namaPengunjung[i]);
+        }
+    }
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+}
